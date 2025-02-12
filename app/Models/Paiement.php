d1a73514b8075531a93170d2f35b510a09f2a906
kaoutarlabjioui <kalabjioui@gmail.com>
2025-02-13 00:25:16 +0100

    add class paiement

---
+++ app/Models/Paiement.php
@@ -0,0 +1,25 @@
+<?php 
+namespace app\Models;
+use app\Config\Database;
+use PDO;
+
+
+class Paiement{
+
+private $id;
+private $amount;
+private User $client;
+private User $freelancer;
+private $status;
+private $type;
+
+
+public function __construct(){}
+
+
+
+
+
+
+
+}
