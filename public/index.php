76d54e0b9bd15352ea4f6649f18062dcd57c2ef8
Ameksa khadija <ameksakhadija026@gmail.com>
2025-02-13 10:42:41 +0100

    modifier profile

--- public/index.php
+++ public/index.php
@@ -2,10 +2,7 @@
 require_once dirname(__DIR__) . "\\vendor\\autoload.php";
 
 include '../app/core/Router.php';
-<<<<<<< HEAD
-=======
 session_start();
->>>>>>> b3c9cc8529f6f8c245675d47f1f1873a1a838ec8
 // session_unset();
 
 
